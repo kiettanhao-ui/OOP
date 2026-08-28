ff5e3bea6428f61e9de4f96ffd194020a4331ef1
kiettanhao-ui <kiettanhao@gmail.com>
2026-08-28 14:47:50 +0700

    Delete BookStore directory

--- BookStore/src/TestBook.java
+++
@@ -1,9 +0,0 @@
-import junit.framework.TestCase;
-public class TestBook extends TestCase{
-    public void testConstructor()
-    {
-            new Book("Daniel Defoe","Robinson Crusoe", 15.50, 1719);
-            new Book("Joseph Conrad","Heart of Darkness", 12.80, 1902);
-            new Book("Pat Conroy", "Beach Music", 9.50, 1996);
-    }
-}
