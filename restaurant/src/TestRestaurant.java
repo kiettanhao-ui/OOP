84abdf8b403dc0c76c532781330f603293a248c2
kiettanhao-ui <kiettanhao@gmail.com>
2026-08-28 14:48:54 +0700

    Delete restaurant directory

--- restaurant/src/TestRestaurant.java
+++
@@ -1,6 +0,0 @@
-import junit.framework.TestCase;
-public class TestRestaurant extends TestCase {
-    public void testConstructor(){
-        Restaurant r1=new Restaurant("La Crepe","French food","Moderate",new Intersection("7th Ave ","65th Street"));
-    }
-}
